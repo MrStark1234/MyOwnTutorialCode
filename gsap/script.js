4b79a5b1c26111410a4a3906e58f99376e9c1f53
Suraj Singhal <117528760+MrStark1234@users.noreply.github.com>
2024-02-07 00:10:00 +0530

    landing page

--- gsap/script.js
+++ gsap/script.js
@@ -1,34 +1,63 @@
 var tl = gsap.timeline();
 
 tl.from("#nav img, #nav-2 h3, #nav-3 h3,button ", {
-  y: -6,
+  y: -16,
   duration: 1,
   delay: 0.5,
   opacity: 0,
   stagger: 0.2,
 });
-tl.from("#main h1", {
+tl.from("#page-1 h1", {
   y: 100,
   duration: 2,
   opacity: 0,
   stagger: 0.2,
 });
 
-tl.from("#img-1, #img-4", {
-  x: -400,
+tl.from("#img-1, #img-4, #img-3, #img-2", {
+  scale: 0,
   duration: 2,
   opacity: 0,
   stagger: 0.2,
 });
-tl.from("span", {
+tl.from("#page-1 span", {
   duration: 2,
   opacity: 0,
 });
 
-gsap.from("#img-2, #img-3", {
-  x: 400,
-  duration: 2,
-  delay: 5,
+tl.from("h5", {
   opacity: 0,
-  stagger: 0.2,
+  scale: 0,
 });
+tl.to("h5", {
+  y: 20,
+  repeat: -1,
+  yoyo: true,
+});
+
+// gsap.from(".page-2 p", {
+//   opacity: 0,
+//   scale: 0,
+//   duration: 2,
+//   scrollTrigger: {
+//     trigger: ".page-2 p",
+//     scroller: "body",
+//     markers: true,
+//     start: "bottom 90%",
+//     end: "top 20%",
+//     scrub: true,
+//   },
+// });
+// gsap.from(".page-3 p", {
+//   opacity: 0,
+//   scale: 0,
+//   duration: 2,
+//   scrollTrigger: {
+//     trigger: ".page-3 p",
+//     scroller: "body",
+//     markers: true,
+//     // start: "bottom 90%",
+//     // end: "top 20%",
+//     scrub: true,
+//   },
+// });
